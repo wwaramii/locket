d0746daa28107bff917bfcafc8ce8914f1e0c6c3
wwaramii <wwaramii@gmail.com>
2024-05-21 19:40:22 +0330

    [implement] database middleware

---
+++ safe_pass/middlewares/database.py
@@ -0,0 +1,20 @@
+from typing import Any, Awaitable, Callable, Dict
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from safe_pass.db.base import DBBase
+
+class DatabaseMiddleware(BaseMiddleware):
+    """
+    this middleware will pass each handler the initialized database as an argument.
+    """
+    def __init__(self, database: DBBase) -> None:
+        self.database = database
+        super().__init__()
+    
+    async def __call__(self, 
+                        handler: Callable[[TelegramObject, 
+                        Dict[str, Any]], Awaitable[Any]], 
+                        event: TelegramObject, 
+                        data: Dict[str, Any]) -> Any:
+        return handler(event, data, database=self.database)
