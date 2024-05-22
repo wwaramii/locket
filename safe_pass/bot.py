c46a3e0f440c30cdcd9341a81fc56bf3f3be7bb6
wwaramii <wwaramii@gmail.com>
2024-05-22 10:00:25 +0330

    [ADDED] using proxy for polling

--- safe_pass/bot.py
+++ safe_pass/bot.py
@@ -6,6 +6,7 @@ from .config import config
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.client.session.aiohttp import AiohttpSession
 from safe_pass.middlewares import DatabaseMiddleware
 
 def initialize_logging():
@@ -15,9 +16,15 @@ def initialize_logging():
 def main():
     # logging
     initialize_logging()
+
+    # use proxy
+    session = None
+    if config['proxy']['address']:
+        session = AiohttpSession(proxy=config['proxy']['address'])
     
     # initialize bot
     bot = Bot(token=config["bot"]["TOKEN"],
+              session=session,
               default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
