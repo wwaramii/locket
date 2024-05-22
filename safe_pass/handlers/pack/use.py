4294ca69628de5a4d67533f5fa02e7eac7749cc6
wwaramii <wwaramii@gmail.com>
2024-05-22 18:48:24 +0330

    [ADD] use handlers

--- safe_pass/handlers/pack/use.py
+++ safe_pass/handlers/pack/use.py
@@ -4,10 +4,11 @@ from aiogram.fsm.context import FSMContext
 
 from safe_pass.db.base import DBBase
 from safe_pass.keyboards import InlineConstructor
-from safe_pass.models.base import User
+from safe_pass.models.base import User, Document
 
 from .router import pack_router
 from .login import start_login
+
 
 @pack_router.message(Command("use"))
 async def use_command(message: types.Message, 
@@ -15,20 +16,58 @@ async def use_command(message: types.Message,
                       user: User,
                       state: FSMContext):
     await message.answer(**await use(database, user, state))
-    ...
 
 
-@pack_router.callback_query(F.data == "pack::use")
+@pack_router.callback_query(F.data.startswith("pack::use"))
 async def use_cb(cb: types.CallbackQuery, 
                  database: DBBase, 
                  user: User,
                  state: FSMContext):
-    await cb.message.edit_text(**await use(database, user, state))
+    # paging
+    page = int(cb.data.split("?page=")[-1]) if cb.data.split("?page=")[-1].isnumeric() else 0
+    await cb.answer()
+    try:
+        await cb.message.edit_text(**await use(database, user, state, page=page))
+    except:
+        pass # paging errors
 
 
 async def use(database: DBBase, 
               user: User,
-              state: FSMContext):
+              state: FSMContext,
+              page: int=0):
     if not user.document_pack:
         return await start_login(state)
-    # TODO: handle using a document
+    
+    m = """<b>🔓 You are logged in and have access to your passwords.</b>
+- You will stay logged in for 5 minutes.
+- You can logout and access other packs.
+
+<b>📌 All passwords in this pack are listed.</b> You can access each one by tapping it:"""
+    # prepare buttons
+    buttons = [
+        {"text": "➕ Add password", "callback_data": "documents::add"},
+        {"text": "🚪 Logout", "callback_data": "pack::logout"}
+    ]
+    schema = [2]
+    
+    async for document in database.read_many_docs({
+        'document_pack_identifier': user.document_pack.identifier},
+        start=page*10,
+        end=(page*10) + 10):
+        buttons.append({
+            "text": f"• {document.title}",
+            "callback_data": f"documents::view?id={document.id}"
+        })
+        schema.append(1)
+    buttons.extend([
+        {"text": "⬅️ Last page", "callback_data": f"packs::use{page-1}"},
+        {"text": "➡️ Next page", "callback_data": f"pack::use?page={page+1}"},
+        {"text": "🔚 Cancel", "callback_data": "globals::cancel"}
+    ])
+    schema.extend([2, 1])
+
+    return {
+        "text": m,
+        "reply_markup": InlineConstructor._create_kb(buttons, schema)
+    }
